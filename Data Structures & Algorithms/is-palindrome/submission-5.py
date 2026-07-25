46bdeb1515d76c328635c85672ba5ef7a7b3e7a8
Hossein Moein <moeinhochen@gmail.com>
2026-07-25 08:17:14 +0330

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ls = ""
+        for i in s:
+            if i.isalnum():
+                 ls += i.lower()
+
+
+
+        return ls == ls[::-1]
+
+
+        
+        
+        
